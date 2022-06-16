bf18f30181c306d33eff0e9dab60922765034aae
Nathalie <xnhogenbergx@gmail.com>
2022-06-16 11:39:44 -0700

    clipping solved
    projecting previous positions as the new position when the ray is hitting an object

---
+++ Assets/scripts/AntiClipping.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntiClipping : MonoBehaviour
+{
+    public LayerMask collidable;
+    public new Collider collider;
+    public float maxOffser = 0.05f;
+
+    [HideInInspector] public bool blockedForward = false;
+    [HideInInspector] public bool blockedUp = false;
+    [HideInInspector] public bool blockedDown = false;
+    [HideInInspector] public bool blockedBack = false;
+    [HideInInspector] public bool blockedLeft = false;
+    [HideInInspector] public bool blockedRight = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        blockedForward = StopMoving(Vector3.forward).collider != null;
+        blockedUp = StopMoving(Vector3.up).collider != null;
+        blockedDown = StopMoving(Vector3.down).collider != null;
+        blockedBack = StopMoving(Vector3.back).collider != null;
+        blockedLeft = StopMoving(Vector3.left).collider != null;
+        blockedRight = StopMoving(Vector3.right).collider != null;
+    }
+
+    public RaycastHit StopMoving(Vector3 direction)
+    {
+        if (transform.position == null) return new RaycastHit();
+
+        Vector3 origin = transform.position;
+        RaycastHit hit;
+        Physics.Raycast(origin, direction, out hit, collider.bounds.extents.x + maxOffser);
+        Debug.DrawRay(origin, direction * (collider.bounds.extents.x + maxOffser));
+
+        return hit;
+    }
+}
