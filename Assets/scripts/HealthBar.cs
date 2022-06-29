2c083fa338d5df12779d32e2fb62500fece8a93b
Nathalie <xnhogenbergx@gmail.com>
2022-06-29 11:48:50 -0700

    stuff
    making it work with code

--- Assets/scripts/HealthBar.cs
+++ Assets/scripts/HealthBar.cs
@@ -7,14 +7,14 @@ public class HealthBar : MonoBehaviour
 {
     public Slider slider;
 
-    public void SetHealth(int health)
-    {
-        slider.value = health;
-    }
-
     public void MaxHealth(int health)
     {
         slider.maxValue = health;
+        slider.value = health;
+    }
+
+    public void SetHealth(int health)
+    {
         slider.value = health;
     }
 }
